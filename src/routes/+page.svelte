b36c017f857f121c9e81bf2208c150f913758c22
Nitish <nitishethan@gmail.com>
2024-02-24 23:08:50 -0600

    CSS is ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,6 +74,8 @@
 		if (e.key !== 'Enter') return;
 
 		const inputEl = e.target as HTMLInputElement;
+		if (!inputEl.value) return;
+
 		tasks.push(inputEl.value);
 
 		inputEl.value = '';
@@ -91,6 +93,8 @@
 				dailyTaskLists[i].taskList.push(false);
 			}
 		}
+
+		inputEl.blur();
 	}
 
 	function removeTask(index: number) {
@@ -119,69 +123,64 @@
 	}
 </script>
 
-<header><h1>{currMonthString}</h1></header>
+<header>
+	<h1 class="margin-bottom: 0;">{currMonthString}</h1>
+	<input onkeydown={addTask} placeholder="Add daily tasks" type="text" class="add-task" />
+</header>
 
 <main>
-	<input onkeydown={addTask} placeholder="Add daily tasks" type="text" />
-	<div class="container">
-		<div style="display: flex; flex-direction: row;">
-			<table class="striped">
-				<thead>
-					<tr>
-						<th scope="col">Date</th>
-						<th scope="col">Highlight</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#if days}
-						{#each days.slice().reverse() as day, i}
-							{#if compareToday(days[days.length - 1 - i].UTCDate)}
-								<tr>
-									<th scope="row">{day.date}</th>
-									<td>
-										<textarea
-											onkeydown={blurFocus}
-											on:input={limitCharacters}
-											bind:value={day.highlight}
-											class="highlight"
-										>
-										</textarea>
-									</td>
-								</tr>
-							{/if}
-						{/each}
-					{/if}
-				</tbody>
-			</table>
-			<div class="scrollable">
-				<div class="task-list-row">
-					{#each tasks as task, i}
+	<div style="display: flex; flex-direction: row; gap: 2%;">
+		<table style="flex: 1;">
+			<thead>
+				<tr>
+					<th scope="col" class="date">Date</th>
+					<th scope="col" class="highlight">Highlight</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr style="height: 5px;"></tr>
+				{#if days}
+					{#each days.slice().reverse() as day, i}
+						{#if compareToday(days[days.length - 1 - i].UTCDate)}
+							<tr>
+								<th scope="row" class="date">{day.date}</th>
+								<td>
+									<textarea
+										onkeydown={blurFocus}
+										on:input={limitCharacters}
+										bind:value={day.highlight}
+										class="highlight"
+									>
+									</textarea>
+								</td>
+							</tr>
+						{/if}
+					{/each}
+				{/if}
+			</tbody>
+		</table>
+		<div class="scrollable" style="flex: 1;">
+			<div class="task-list-row">
+				{#each tasks as task, i}
+					<div class="task-list-col">
 						<div class="group">
 							<input onkeydown={blurFocus} bind:value={tasks[i]} />
 							<button onclick={() => removeTask(i)} class="delete-button">
 								<span class="material-symbols-outlined"> delete </span>
 							</button>
 						</div>
-					{/each}
-				</div>
-				{#if days}
-					<div class="tasks-list-row">
-						{#each tasks as task, i}
-							<div class="task-list-col">
-								{#each days.slice().reverse() as day, j}
-									{#if compareToday(days[days.length - 1 - j].UTCDate)}
-										<div class="checkbox-container">
-											<input
-												type="checkbox"
-												bind:checked={dailyTaskLists[days.length - 1 - j].taskList[i]}
-											/>
-										</div>
-									{/if}
-								{/each}
-							</div>
+						{#each days.slice().reverse() as day, j}
+							{#if compareToday(days[days.length - 1 - j].UTCDate)}
+								<div class="checkbox-container">
+									<input
+										type="checkbox"
+										bind:checked={dailyTaskLists[days.length - 1 - j].taskList[i]}
+									/>
+								</div>
+							{/if}
 						{/each}
 					</div>
-				{/if}
+				{/each}
 			</div>
 		</div>
 	</div>
